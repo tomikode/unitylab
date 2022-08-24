801c3cec8717f71c0aa976c48fb764c562ab9c04
tomikode <tomi.piisila@gmail.com>
2022-08-24 20:33:10 +1000

    80 percent

---
+++ Assets/Scripts/ConsolePrint.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsolePrint : MonoBehaviour
+{
+
+    int i;
+    // Start is called before the first frame update
+    void Start()
+    {
+        i = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        i++;
+        Debug.Log($"{gameObject.name}:{i}");
+    }
+}
